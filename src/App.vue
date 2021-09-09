c2fb78c1ccaa341956bf5283d8684516d707cf05
Guilherme Assunção <cgmatosassuncao@gmail.com>
2021-09-09 19:05:20 -0300

    Vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <Header />
+        <router-view></router-view>
     </div>
-    <router-view/>
-  </div>
 </template>
 
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+    components: {
+        Header,
+    },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+    margin: 0;
+    font-family: "jet";
+    color: #234;
 }
 
-#nav {
-  padding: 30px;
+ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+a {
+    font-weight: bold;
+    text-decoration: none;
+    color: #234;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+    <header>
+        <nav>
+            <div class="header-logo">
+                <router-link to="/">Techno</router-link>
+            </div>
+            <ul>
+                <li><router-link to="/cursos">Cursos</router-link></li>
+                <li><router-link to="/contato">Contato</router-link></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+};
+</script>
+
+<style scoped>
+header {
+    background: #234;
+}
+
+.header-logo {
+    flex: 1;
+}
+
+.header-logo a {
+    color: white;
+    font-size: 1.25rem;
+}
+
+nav {
+    max-width: 800px;
+    margin: 0 auto;
+    display: flex;
+    align-content: space-between;
+    padding: 20px;
+}
+
+nav ul li {
+    display: inline-block;
+}
+
+nav li a {
+    padding: 0 10px;
+    margin-left: 10px;
+    color: white;
+}
+
+nav li a.router-link-exact-active {
+    color: #4b8;
+}
+</style>
